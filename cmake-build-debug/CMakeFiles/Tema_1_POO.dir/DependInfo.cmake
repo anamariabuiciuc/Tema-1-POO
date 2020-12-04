d699cc3fcdc017910bb68d21f3b68636b344012b
anamariabuiciuc <anamariab1612@gmail.com>
2020-12-04 17:36:54 +0200

    //

---
+++ cmake-build-debug/CMakeFiles/Tema_1_POO.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "F:/facultate/Anul 2/POO/Tema 1 POO/Client.cpp" "F:/facultate/Anul 2/POO/Tema 1 POO/cmake-build-debug/CMakeFiles/Tema_1_POO.dir/Client.cpp.obj"
+  "F:/facultate/Anul 2/POO/Tema 1 POO/Comanda.cpp" "F:/facultate/Anul 2/POO/Tema 1 POO/cmake-build-debug/CMakeFiles/Tema_1_POO.dir/Comanda.cpp.obj"
+  "F:/facultate/Anul 2/POO/Tema 1 POO/Produs.cpp" "F:/facultate/Anul 2/POO/Tema 1 POO/cmake-build-debug/CMakeFiles/Tema_1_POO.dir/Produs.cpp.obj"
+  "F:/facultate/Anul 2/POO/Tema 1 POO/main.cpp" "F:/facultate/Anul 2/POO/Tema 1 POO/cmake-build-debug/CMakeFiles/Tema_1_POO.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
