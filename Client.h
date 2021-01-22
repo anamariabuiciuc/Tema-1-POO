ed8fd88fbd76e867fe86262200793ca6fb0d83a2
anamariabuiciuc <63802995+anamariabuiciuc@users.noreply.github.com>
2021-01-22 21:07:14 +0200

    builder

--- Client.h
+++ Client.h
@@ -7,30 +7,46 @@
 
 
 class Client {
+
     std::string nume;
     Comanda comand; //clientul are o comanda
     int varsta;
-    
-     private:
-    
-        Client()=default;
-    
-        static Client* clientul;
 
 public:
 
- //   Client(const std::string, Comanda &, const int);
-    
-    {
-        if (clientul== nullptr){ clientul= new Client;}
-        return clientul;
-    };
+    Client(const std::string, Comanda &, const int);
+
+    void set_nume(const std::string &nume) {
+        Client::nume = nume;
+    }
+
 
     int ClientMinor();
 
     void afisarePlata();
 
+
+    virtual ~Client() {
+       // std::cout << "destruct Client\n";
+    }
 };
+
+
+class clientBuilder {
+    Client cl;
+public:
+    clientBuilder& nume(std::string nume) {
+
+        cl.set_nume(nume);
+        return *this;
+    }
+
+    Client build() { return cl; }
+
+
+
+};
+
 
 
 #endif //TEMA2_CLIENT_H
